f5965606894f3ee14b13d60cc9b1fe0f552fbfae
Jeff030197 <edwardparias3@gmail.com>
2023-10-16 12:05:26 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Navbar from './Components/Header/Navbar';
 import SearchBar from './Components/Body/SearchBar';
 import Cards from './Components/Body/Cards';
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <SearchBar/>
     <Cards/>
+    <Footer/>
+
     </div>
   );
 }
