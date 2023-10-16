55a00001c5f28e7e9a96bac9ee521fcd855972c8
Jeff030197 <edwardparias3@gmail.com>
2023-10-16 12:03:54 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Header/Navbar';
 import SearchBar from './Components/Body/SearchBar';
+import Cards from './Components/Body/Cards';
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <SearchBar/>
+    <Cards/>
     </div>
   );
 }
